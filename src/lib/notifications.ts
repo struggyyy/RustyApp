3262ed0302d27c9748bbeb62995c278c5e6baad1
struggyyy <strugalajacob@gmail.com>
2025-12-06 13:49:37 +0100

    Small warn fix

--- src/lib/notifications.ts
+++ src/lib/notifications.ts
@@ -30,7 +30,6 @@ import { translate, translateStatus } from "@/shared/utils/serverTranslations";
 // Configure notification handler
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
-    shouldShowAlert: true,
     shouldPlaySound: true,
     shouldSetBadge: false,
     shouldShowBanner: true,
